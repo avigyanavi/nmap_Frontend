de75647fee744dc23e4bc0e3c6625cb7b52987db
RoughPatch1 <mukher58@purdue.edu>
2022-12-04 19:25:45 -0500

    Revert "Changed views.py - home(), added API"
    This reverts commit 94e1427d2d62ff78b7f7ac1f59abc2c37ce7e4cb.

--- nhome/views.py
+++ nhome/views.py
@@ -11,9 +11,7 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes, force_str
 from django.contrib.auth import authenticate, login, logout
 from django.template import loader
-import re
-import json
-from urllib2 import urlopen
+
 
 def index(request):
     return render(request, "nhome/index.html")
@@ -25,14 +23,8 @@ def signin(request):
     return render(request, "nhome/signin.html", context)
 
 
-
 def home(request):
     context = {}
-    ip = '10.186.68.122';
-    response = urlopen('http://ipwho.is/'+ip)
-    ipwhois = json.load(response)
-
-    print '{0} {1}'.format(ipwhois['country'],ipwhois['flag']['emoji'])
     return render(request, 'nhome/home.html', context)
 # from django.template import loader
 # from django.http import HttpResponse
@@ -57,4 +49,3 @@ def aboutme(request):
 def hosts(request):
     context = {}
     return render(request, 'nhome/hosts.html', context)
-
